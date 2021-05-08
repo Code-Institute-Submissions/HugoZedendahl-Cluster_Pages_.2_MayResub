49189dadbcee9e37ba2f16951c05f96a4111d92c
HugoZedendahl <hugo.zedendahl@gmail.com>
2021-05-08 20:18:55 +0000

    added triggers for buttons

--- assets/js/clusterData.js
+++ assets/js/clusterData.js
@@ -1,3 +1,51 @@
 var runtimeCluster = [];
 var number;
 var clusterNumbers = [1, 2, 3, 4, 5, 6];
+/* prevents functions to be launched before dom is ready*/
+$( document ).ready(function() 
+{
+    $(".inputButton").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+
+        }
+    );
+
+$(".addURL").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            storeUrlCluster();
+        }
+    );
+
+$(".resetCluster").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            resetCluster();
+        }
+    );
+
+$(".nameCluster").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            nameClusterButton();
+        }
+    );
+
+$(".launch").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            launchCluster();
+        }
+    );
+}
